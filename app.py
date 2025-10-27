2ceef69dcdaadd1daac8e4b141beff98d2c0d6d4
Sopatun <zopatulin@gmail.com>
2025-10-27 15:48:14 -0600

    Mensaje

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 #from routes.clientes import clientes_bp
 #from routes.dvds import dvds_bp
 from routes.rental import rental_bp
 #from routes.staff import staff_bp
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 # Registrar rutas (Blueprints)
 #app.register_blueprint(clientes_bp)
