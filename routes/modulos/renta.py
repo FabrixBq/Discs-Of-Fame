dac5fcee9d5a4de352f6b9645e19d5a18c24f511
Sopatun <zopatulin@gmail.com>
2025-11-02 22:19:06 -0600

    nuevos archivos python

--- routes/modulos/renta.py
+++ routes/modulos/renta.py
@@ -15,7 +15,11 @@ def get_renta():
     cur = conn.cursor()
     cur.execute("SELECT c.customer_id AS id_cliente, c.first_name || ' ' || c.last_name AS nombre, f.title AS pelicula, r.rental_date AS fecha_renta, r.return_date AS fecha_devolucion, CASE WHEN r.return_date IS NULL THEN 'En Renta' ELSE 'Devuelto' END AS estado FROM rental r JOIN customer c ON r.customer_id = c.customer_id JOIN inventory i ON r.inventory_id = i.inventory_id JOIN film f ON i.film_id = f.film_id ORDER BY r.rental_date DESC;")
 
-    rows = cur.fetchall()
+    rows = cur.fetchall() #Extrae todas las filas
+    #rows = cur.fetchone() #Extrae una sola fila
+    #rows = cur.fetchmany(10) #Obtiene un numero especifico de filas, en este caso 10
+    #El cursor es iterable, si miras que tarda, puedes remplazar estos metodos por un ciclo for y 
+    #de ahi hacer las iteracciones necesarias, por ejemplo,añadir los datos en el diccionario
     
     cur.close()
     conn.close()
